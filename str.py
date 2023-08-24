26c827c24c0f23b4fd550ae9028d4e971597b097
hc6078 <571574836@qq.com>
2023-08-24 11:09:23 +0800

    Create str.py

---
+++ str.py
@@ -0,0 +1,13 @@
+if __name__ == '__main__':
+    str = '1234'
+    str_1 = "1234"
+    print(str, str_1)
+    str_2 = '''ok 
+    hello '''
+    print(str_2)
+    str_3 = "WQSD"
+    print(len(str_3))  # j计算字符串长度 4
+    print(str_3.lower(), str_3.upper())  ## 字符串转小写，大写
+    str_4 = str_1 + str_3  # 字符串相加
+    print(str_4)
+
