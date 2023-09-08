2f7b2039a2d9fad21b43a09267333594366b656d
hc6078 <571574836@qq.com>
2023-09-08 17:40:03 +0800

    列表推导式添加元素

--- list.py
+++ list.py
@@ -22,3 +22,10 @@ if __name__ == '__main__':
     fruits2 = fruits[1:4]  # 跟golang一样，前闭后开 ,取不到4下标
     print(fruits2)  # apple strawberry waxberry
     fruits3 = sorted(fruits2)  # 列表排序
+
+    # 列表推导式添加元素
+    lst = [i for i in range(1, 11)]
+    print(lst) # 输出结果为： [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    
+
+
